e5ea7f8a7eb52dc2c5c974b27a4d1ce21d704788
beyondtheinferno <40118538+beyondtheinferno@users.noreply.github.com>
2019-03-28 22:06:57 +0530

    Added Auth Screen Log In / Sign Up page with validation and styling is implemented.

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,23 +1,247 @@
-import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ImageBackground,
+  Dimensions
+} from "react-native";
 
 import startMainTabs from './MainTabs/startMainTabs';
+import DefaultInput from "../../src/components/UI/DefaultInput";
+import HeadingText from "../../src/components/UI/HeadingText";
+import MainText from "../../src/components/UI/MainText";
+import ButtonWithBackground from '../../src/components/UI/ButtonWithBackground';
+import validate from '../utility/validation';
+
 
 export default class AuthScreen extends Component {
+    state = {
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+        authMode: "login",
+        controls: {
+          email: {
+            value: "",
+            valid: false,
+            validationRules: {
+              isEmail: true
+            },
+            touched: false
+          },
+          password: {
+            value: "",
+            valid: false,
+            validationRules: {
+              minLength: 6
+            },
+            touched: false
+          },
+          confirmPassword: {
+            value: "",
+            valid: false,
+            validationRules: {
+              equalTo: "password"
+            },
+            touched: false
+          }
+        }
+      };
+    
+      constructor(props) {
+        super(props);
+        Dimensions.addEventListener("change", this.updateStyles);
+      }
+    
+      componentWillUnmount() {
+        Dimensions.removeEventListener("change", this.updateStyles);
+      }
 
-    loginHandler = () => {
+      switchAuthModeHandler = () => {
+        this.setState(prevState => {
+          return {
+            authMode: prevState.authMode === "login" ? "signup" : "login"
+          }
+        })
+      }
+    
+      updateStyles = dims => {
+        this.setState({
+          viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+        });
+      };
+    
+      loginHandler = () => {
         startMainTabs();
-    }
+      };
+    
+      updateInputState = (key, value) => {
+        let connectedValue = {};
+        if (this.state.controls[key].validationRules.equalTo) {
+          const equalControl = this.state.controls[key].validationRules.equalTo;
+          const equalValue = this.state.controls[equalControl].value;
+          connectedValue = {
+            ...connectedValue,
+            equalTo: equalValue
+          };
+        }
+        if (key === "password") {
+          connectedValue = {
+            ...connectedValue,
+            equalTo: value
+          };
+        }
+        this.setState(prevState => {
+          return {
+            controls: {
+              ...prevState.controls,
+              confirmPassword: {
+                ...prevState.controls.confirmPassword,
+                valid:
+                  key === "password"
+                    ? validate(
+                        prevState.controls.confirmPassword.value,
+                        prevState.controls.confirmPassword.validationRules,
+                        connectedValue
+                      )
+                    : prevState.controls.confirmPassword.valid
+              },
+              [key]: {
+                ...prevState.controls[key],
+                value: value,
+                valid: validate(
+                  value,
+                  prevState.controls[key].validationRules,
+                  connectedValue
+                ),
+                touched: true
+              }
+            }
+          };
+        });
+      };
+    
 
     render () {
+        let headingText = null;
+        let confirmPasswordControl = null;
+
+        if (this.state.viewMode === "portrait") {
+            headingText = (
+            <MainText>
+                <HeadingText>Please Log In</HeadingText>
+            </MainText>
+            );
+        }
+
+        if (this.state.authMode === "signup") {
+          confirmPasswordControl = (
+            <View
+            style={
+                this.state.viewMode === "portrait"
+                ? styles.portraitPasswordWrapper
+                : styles.landscapePasswordWrapper
+            }
+            >
+            <DefaultInput
+                placeholder="Confirm Password"
+                style={styles.input}
+                value={this.state.controls.confirmPassword.value}
+                onChangeText={val =>
+                this.updateInputState("confirmPassword", val)}
+                valid={this.state.controls.confirmPassword.valid}
+                touched={this.state.controls.confirmPassword.touched}
+            />
+            </View>
+          );
+        }
         return (
-            <View>
-                <Text>{"Hello World!!"}</Text>
-                <Button 
-                    title="Login"
-                    onPress={this.loginHandler}
+            <View style={styles.container}>
+                {headingText}
+                <ButtonWithBackground 
+                  color="#29aaf4" 
+                  onPress={this.switchAuthModeHandler}>
+                Switch to {this.state.authMode === 'login' ? "Sign Up" : "Login"}
+                </ButtonWithBackground>
+                <View style={styles.inputContainer}>
+                <DefaultInput
+                    placeholder="Your E-Mail Address"
+                    style={styles.input}
+                    value={this.state.controls.email.value}
+                    onChangeText={val => this.updateInputState("email", val)}
+                    valid={this.state.controls.email.valid}
+                    touched={this.state.controls.email.touched}
                 />
+                <View
+                    style={
+                    this.state.viewMode === "portrait"
+                        ? styles.portraitPasswordContainer
+                        : styles.landscapePasswordContainer
+                    }
+                >
+                    <View
+                    style={
+                        this.state.viewMode === "portrait"
+                        ? styles.portraitPasswordWrapper
+                        : styles.landscapePasswordWrapper
+                    }
+                    >
+                    <DefaultInput
+                        placeholder="Password"
+                        style={styles.input}
+                        value={this.state.controls.password.value}
+                        onChangeText={val => this.updateInputState("password", val)}
+                        valid={this.state.controls.password.valid}
+                        touched={this.state.controls.password.touched}
+                    />
+                    </View>
+                    {confirmPasswordControl}
+                </View>
+                </View>
+                <ButtonWithBackground 
+                  color="#29aaf4" 
+                  onPress={this.loginHandler}
+                  disabled={
+                    !this.state.controls.confirmPassword.valid && this.state.authMode === "signup" ||
+                    !this.state.controls.password.valid ||
+                    !this.state.controls.email.valid}>
+                Submit
+                </ButtonWithBackground>
             </View>
         );
-    }
+        }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    backgroundImage: {
+      width: "100%",
+      flex: 1
+    },
+    inputContainer: {
+      width: "80%"
+    },
+    input: {
+      backgroundColor: "#eee",
+      borderColor: "#bbb"
+    },
+    landscapePasswordContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between"
+    },
+    portraitPasswordContainer: {
+      flexDirection: "column",
+      justifyContent: "flex-start"
+    },
+    landscapePasswordWrapper: {
+      width: "45%"
+    },
+    portraitPasswordWrapper: {
+      width: "100%"
+    }
+  });
